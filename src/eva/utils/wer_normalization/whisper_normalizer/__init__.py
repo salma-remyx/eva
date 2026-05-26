b0d3a137e67492a076bafea085525fa3e5817cc3
Katrina <katrina.stankiewicz@servicenow.com>
2026-05-26 10:41:10 -0400

    add french number normalizer

--- src/eva/utils/wer_normalization/whisper_normalizer/__init__.py
+++ src/eva/utils/wer_normalization/whisper_normalizer/__init__.py
@@ -2,5 +2,6 @@
 
 from eva.utils.wer_normalization.whisper_normalizer.basic import BasicTextNormalizer
 from eva.utils.wer_normalization.whisper_normalizer.english import EnglishTextNormalizer
+from eva.utils.wer_normalization.whisper_normalizer.french import FrenchTextNormalizer
 
-__all__ = ["BasicTextNormalizer", "EnglishTextNormalizer"]
+__all__ = ["BasicTextNormalizer", "EnglishTextNormalizer", "FrenchTextNormalizer"]
